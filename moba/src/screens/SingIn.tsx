4a14cc05e623274e24c269733919c2c65b21af5f
mazzillio <mateca500@gmail.com>
2022-11-06 18:56:54 -0300

    Adicionando funcionalidades de svg ao projeto moba

--- moba/src/screens/SingIn.tsx
+++ moba/src/screens/SingIn.tsx
@@ -1,9 +1,15 @@
-import { Center, Text } from "native-base";
-
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
+import Logo from '../assets/logo.svg';
+import { Button } from "../components/Button";
 export function SignIn() {
     return(
     <Center flex={1}>
-        <Text>SignIn</Text>
+        <Logo width={212} height={40}/>
+        <Button 
+            title="Entrar com o google" 
+            leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        />
     </Center>
     )
 }
